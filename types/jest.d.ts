64439a125e1404642a09317e1815b5f198d431f0
SamVerschueren <sam.verschueren@gmail.com>
2022-03-04 16:15:56 +0100

    chore: Update types

--- types/jest.d.ts
+++ types/jest.d.ts
@@ -1,5 +1,11 @@
+type DescribeParams = Parameters<jest.Describe>;
+
+interface PlatformSupport {
+  firefox?: boolean;
+}
+
 declare namespace jest {
-    interface Describe {
-        platform: Describe;
-    }
+  interface Describe {
+    platform(name: DescribeParams[0], support: PlatformSupport, fn: DescribeParams[1]);
+  }
 }
